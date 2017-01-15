8919b5e672f239d97d0e73869aa8028b6267282a
Seneca Sisneros <seneca.sisneros@yahoo.com>
2017-01-15 12:16:44 -0800

    added react-router and results component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {List} from 'immutable';
+
+const pair = List.of('Trainspotting', '28 Days Later');
+
+export default React.createClass({
+  render: function() {
+    return React.cloneElement(this.props.children, {pair: pair});
+  }
+});
+
+// <Voting pair={pair} winnner="Trainspotting"/>,
+  //hasVoted="Trainspotting"
